bcb56acf0408cc636784081078e2acec3296ddfd
Gianluca Elia <34285663+gianlukk994@users.noreply.github.com>
2025-04-17 11:45:56 +0200

    Enable schema dump for multiple metafield owners
    Extends schema dump functionality to include multiple
    metafield owner types, generating a comprehensive
    schema for all metafields in a shop.
    
    Co-authored-by: elia <elia@schito.me>

--- spec/shopify_toolkit/schema_spec.rb
+++ spec/shopify_toolkit/schema_spec.rb
@@ -4,63 +4,86 @@ require "spec_helper"
 
 RSpec.describe ShopifyToolkit::Schema do
   let(:schema) { described_class }
-  let(:shopify_admin_client) { instance_double(ShopifyAPI::Clients::Graphql::Admin) }
   let(:root) { Pathname(Dir.mktmpdir) }
 
+  let(:definitions_by_owner) do
+    { products: [product_definition], articles: [article_definition] }
+  end
+
   before do
-    allow(ShopifyAPI::Clients::Graphql::Admin).to receive(:new).and_return(shopify_admin_client)
     allow(Rails).to receive(:root).and_return(root)
     root.join("config/shopify").mkpath
+
+    ShopifyToolkit::Schema::OWNER_TYPES.each do |owner_type|
+      allow(schema).to receive(:fetch_definitions).with(
+        owner_type: owner_type
+      ).and_return(definitions_by_owner.fetch(owner_type, []))
+    end
   end
 
   describe "#dump!" do
-    it "dumps the schema to a file" do
-      allow(shopify_admin_client).to receive(:query).and_return(
-        OpenStruct.new(
-          code: 200,
-          body: {
-            "data" => {
-              "metafieldDefinitions" => {
-                "nodes" => [
-                  {
-                    "id" => "gid://shopify/MetafieldDefinition/1",
-                    "name" => "My Metafield",
-                    "key" => "my_metafield",
-                    "type" => { "name" => "single_line_text_field" },
-                    "namespace" => "custom",
-                    "description" => "My description",
-                    "validations" => [{ "name" => "min_length", "value" => "1" }, { "name" => "max_length", "value" => "10" }],
-                    "capabilities" => {
-                      "smartCollectionCondition" => { "enabled" => true },
-                      "adminFilterable" => { "enabled" => false },
-                    },
-                    "access" => { "admin" => true, "customerAccount" => false, "storefront" => false },
-                    "ownerType" => "PRODUCT",
-                  },
-                  {
-                    "id" => "gid://shopify/MetafieldDefinition/2",
-                    "name" => "My Metafield 2",
-                    "key" => "my_metafield_2",
-                    "type" => { "name" => "integer" },
-                    "namespace" => "my_namespace",
-                    "description" => nil,
-                    "capabilities" => {
-                      "smartCollectionCondition" => { "enabled" => false },
-                      "adminFilterable" => { "enabled" => true },
-                    },
-                    "access" => { "admin" => true, "customerAccount" => true, "storefront" => true },
-                    "ownerType" => "PRODUCT",
-                  },
-                ]
-              }
-            }
+    let(:product_definition) do
+      {
+        "id" => "gid://shopify/MetafieldDefinition/1",
+        "name" => "My Metafield",
+        "key" => "my_metafield",
+        "type" => {
+          "name" => "single_line_text_field"
+        },
+        "namespace" => "custom",
+        "description" => "My description",
+        "validations" => [
+          { "name" => "min_length", "value" => "1" },
+          { "name" => "max_length", "value" => "10" }
+        ],
+        "capabilities" => {
+          "smartCollectionCondition" => {
+            "enabled" => true
+          },
+          "adminFilterable" => {
+            "enabled" => false
           }
-        )
-      )
+        },
+        "access" => {
+          "admin" => true,
+          "customerAccount" => false,
+          "storefront" => false
+        },
+        "ownerType" => "PRODUCT"
+      }
+    end
 
-      expect {  schema.dump! }.to output(/Generating schema/).to_stdout
+    let(:article_definition) do
+      {
+        "id" => "gid://shopify/MetafieldDefinition/2",
+        "name" => "My Metafield 2",
+        "key" => "my_metafield_2",
+        "type" => {
+          "name" => "integer"
+        },
+        "namespace" => "my_namespace",
+        "description" => nil,
+        "capabilities" => {
+          "smartCollectionCondition" => {
+            "enabled" => false
+          },
+          "adminFilterable" => {
+            "enabled" => true
+          }
+        },
+        "access" => {
+          "admin" => true,
+          "customerAccount" => true,
+          "storefront" => true
+        },
+        "ownerType" => "ARTICLE"
+      }
+    end
 
-      expect(root.join("config/shopify/schema.rb").read).to eq(<<~RUBY
+    it "dumps the schema to a file" do
+      expect { schema.dump! }.to output(/Generating schema/).to_stdout
+
+      expect(root.join("config/shopify/schema.rb").read).to eq(<<~RUBY)
         # This file is auto-generated from the current state of the Shopify metafields.
         # Instead of editing this file, please use the metafields migration feature of ShopifyToolkit
         # to incrementally modify your metafields, and then regenerate this schema definition.
@@ -69,11 +92,10 @@ RSpec.describe ShopifyToolkit::Schema do
         #
         # It's strongly recommended that you check this file into your version control system.
         ShopifyToolkit::Schema.define do
+          create_metafield :articles, :my_metafield_2, :integer, name: "My Metafield 2", namespace: :my_namespace, capabilities: {:smartCollectionCondition=>{:enabled=>false}, :adminFilterable=>{:enabled=>true}}
           create_metafield :products, :my_metafield, :single_line_text_field, name: "My Metafield", description: "My description", validations: [{:name=>"min_length", :value=>"1"}, {:name=>"max_length", :value=>"10"}], capabilities: {:smartCollectionCondition=>{:enabled=>true}, :adminFilterable=>{:enabled=>false}}
-          create_metafield :products, :my_metafield_2, :integer, name: "My Metafield 2", namespace: :my_namespace, capabilities: {:smartCollectionCondition=>{:enabled=>false}, :adminFilterable=>{:enabled=>true}}
         end
       RUBY
-      )
     end
   end
 end
